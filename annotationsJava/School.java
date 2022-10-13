b4af1a8a1c0dd3e198e75d448938ae38f814feab
E2-Priya <109967357+E2-Priya@users.noreply.github.com>
2022-10-13 18:38:14 +0530

    annotations without xml

---
+++ annotationsJava/School.java
@@ -0,0 +1,10 @@
+package annotationsJava;
+
+import org.springframework.stereotype.Component;
+
+@Component("school")
+public class School {
+public void schoolMethod() {
+	System.out.println("ABC school");
+}
+}
---
+++ annotationsJava/SchoolConfig.java
@@ -0,0 +1,12 @@
+package annotationsJava;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="annotationsJava")
+public class SchoolConfig {
+
+
+}
